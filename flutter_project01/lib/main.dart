204085628c76e0e67c3eb9772c1983e8c2c2506c
kshoon99 <kshun08@dongguk.edu>
2023-09-22 14:47:35 +0900

    feat: add the contents practiced in lectures 05(about flutter layout 01)

--- flutter_project01/lib/main.dart
+++ flutter_project01/lib/main.dart
@@ -1,42 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Study To Container"),
-          ),
-          body: CustomContainer(),
-        ),
-
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("위젯 상하좌우로 배치"),
+      ),
+      body: Body(),
+    ),
+  ));
 }
 
-class CustomContainer extends StatelessWidget {
-  const CustomContainer({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 100,
-      // color: Colors.red.shade200,
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      margin: EdgeInsets.symmetric(vertical: 12,horizontal: 12),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(color: Colors.black.withOpacity(0.3), offset: Offset(6,6), blurRadius: 10, spreadRadius: 10),
-          BoxShadow(color: Colors.blue.withOpacity(0.3), offset: Offset(-6,-6), blurRadius: 10, spreadRadius: 10),
-        ]
-      ),
-      child: Center(child: Container(
-          color: Colors.yellow,
-          child: Text("Hello Container"))),
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 80,
+                  color: Colors.red,
+                  child: Text('Container 01'),
+                ),
+                Container(
+                  width: 100,
+                  height: 80,
+                  color: Colors.blue,
+                  child: Text('Container 02'),
+                ),
+                Container(
+                  width: 100,
+                  height: 80,
+                  color: Colors.green,
+                  child: Text('Container 03'),
+                ),
+              ],
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+            Container(
+              width: 300,
+              height: 120,
+              color: Colors.black26,
+              child: Text("Container 04"),
+            ),
+
+          ],
+
+        )
     );
   }
 }
