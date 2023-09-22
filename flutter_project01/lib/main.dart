57bfee8612b5cbe704c43d75cf03e51c580b337b
kshoon99 <kshun08@dongguk.edu>
2023-09-22 13:48:04 +0900

    feat: add the contents practiced in lectures 04(about flutter widget 03)

--- flutter_project01/lib/main.dart
+++ flutter_project01/lib/main.dart
@@ -5,40 +5,38 @@ void main(){
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            actions: [
-              IconButton(icon: Icon(Icons.home), onPressed: (){
-                print("Tab!");
-              },),
-              Icon(Icons.play_arrow)
-            ],
-            centerTitle: false,
-            title: Text('This is App Bar')
+            title: Text("Study To Container"),
           ),
-          body: TestWidget(),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.bug_report),
-            onPressed: (){
-              print("Tab! FAB!");
-            },
-          ),
-        )
+          body: CustomContainer(),
+        ),
+
     )
   );
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-        child: Center(
-          child: Text("Hello, Flutter", style: TextStyle(
-            fontSize: 60,
-            color: Colors.black
-    )),
-        ),
-    ));
+    return Container(
+      width: double.infinity,
+      height: 100,
+      // color: Colors.red.shade200,
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      margin: EdgeInsets.symmetric(vertical: 12,horizontal: 12),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(color: Colors.black.withOpacity(0.3), offset: Offset(6,6), blurRadius: 10, spreadRadius: 10),
+          BoxShadow(color: Colors.blue.withOpacity(0.3), offset: Offset(-6,-6), blurRadius: 10, spreadRadius: 10),
+        ]
+      ),
+      child: Center(child: Container(
+          color: Colors.yellow,
+          child: Text("Hello Container"))),
+    );
   }
 }
