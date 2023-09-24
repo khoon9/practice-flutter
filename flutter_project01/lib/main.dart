c3b958b6b9cabb970584758018803d10e150093e
kshoon99 <kshun08@dongguk.edu>
2023-09-24 21:31:43 +0900

    feat: add the contents practiced in lectures 09(about flutter stateful widget)

--- flutter_project01/lib/main.dart
+++ flutter_project01/lib/main.dart
@@ -1,44 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomeWidget(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+      ),
+      body: Body(),
+    ),
   ));
 }
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(body: ConstraintsWidget()),
+    return const Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(index: 3),
+      ],
     );
   }
 }
 
-class ConstraintsWidget extends StatelessWidget {
-  const ConstraintsWidget({super.key});
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-            child: Container(
-                color: Colors.blue,
-                child: Text(
-                  'HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!',
-                  style: TextStyle(fontSize: 30),
-                ))),
-        Expanded(
-            child: Container(
-                color: Colors.red,
-                child: Text(
-                  'HelloWorld!',
-                  style: TextStyle(fontSize: 30),
-                ))),
-      ],
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+
+  const ExampleStateful({required this.index,super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+class _ExampleStatefulState extends State<ExampleStateful> {
+  // 언더바 는 private 변수를 의미한다
+  late int _index;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textEditingController = TextEditingController();
+  }
+
+  // 이 안에서 Stream 을 사용하거나 다른 네트워크 통신 객체를 가져가서 쓴다던지
+  // 스크롤 컨트롤러나 텍스트 컨트롤러 객체들이 상황 종결임을 알려주는 역할을 수행하여 리소스를 반환
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // GestureDetector 란 위젯안에서 특정한 엑션에 대해 감지하는 역할을 수행한다.
+    return Expanded(
+      child: GestureDetector(
+        onTap: (){
+          // state 가 바뀌었다는 걸 위젯한테 알려준다.
+          setState(() {
+
+          });
+          if(_index == 5){
+            _index = 0;
+            return;
+          }
+          _index++;
+        },
+        child: Container(
+        color: Colors.green.withOpacity(_index/5),
+          child: Center(
+            child: Text('$_index'),
+          ),
+        ),
+      ),
     );
   }
 }
