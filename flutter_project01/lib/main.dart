b98735761cc32f60d50a0891c16ecb8941b5e71b
kshoon99 <kshun08@dongguk.edu>
2023-09-28 21:25:04 +0900

    feat: add the contents practiced in lectures 15(about flutter navigator 01)

--- flutter_project01/lib/main.dart
+++ flutter_project01/lib/main.dart
@@ -6,24 +6,66 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter/**/'),
-      ),
-      body: Body(),
-    ),
+    home: HomeWidget(),
   ));
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset('assets/images/banner.png'),
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+      ),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Person'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex)=>setState(() => index = newIndex),
+      ),
     );
   }
-}
 
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return Center(
+            child: Icon(
+          Icons.search,
+          size: 100,
+        ));
+      case 2:
+        return Center(
+            child: Icon(
+          Icons.person,
+          size: 100,
+        ));
+      case 0:
+      default:
+        return Center(
+            child: Icon(
+          Icons.home,
+          size: 100,
+        ));
+    }
+  }
+}
