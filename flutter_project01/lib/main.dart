7d794ee8381546997d38c40fbf087c4b7205a9fe
kshoon99 <kshun08@dongguk.edu>
2023-09-28 19:55:58 +0900

    feat: add the contents practiced in lectures 13(about flutter setting 01)

--- flutter_project01/lib/main.dart
+++ flutter_project01/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,71 +20,20 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TestWidget();
+    return RandomWords();
   }
 }
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
-
-  @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-  late int value;
-
-  @override
-  void initState() {
-    super.initState();
-    value = 0;
-  }
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text(
-        'Count : $value',
-        style: const TextStyle(fontSize: 30),
-      ),
-      // addCounter 메소드 그 자체를 넘겨주는 것이다.
-      TestButton(addCounter),
-    ]);
-  }
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList.map((word)=> Text(word.asCamelCase, style: const TextStyle(fontSize: 32),),).toList();
 
-  // statefull 에서 setState 을 포함한 메소드를 선언하고
-  // stateless 에서 그 메소드를 호출할 수 있도록 할 수 있음을 보여준다.
-  // void addCounter() => setState(() => value++);
-  void addCounter(int addValue) => setState(() => value+=addValue);
-
-}
-
-class TestButton extends StatelessWidget {
-  // 네임드 파라미터외에 밖에다가 callback 선언
-  const TestButton(this.callback, {super.key});
-
-  // 반환값이 없는 콜벡으로 넘겨줄 때 가장 많이 사용
-  // final VoidCallback callback;
-  final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        width: double.infinity,
-        // inkwell 또한 있다. 이는 클릭시 화면이 번지는 듯한 애니메이션이 기본으로있어서 빠른 코딩 용이
-        // GestureDetector 가 더 다양한 기능이 존재.
-        // onTap 과 longTap 이 자주 쓰임
-        child: InkWell(
-          onTap: () => callback.call(5),
-            child: Center(
-              child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: const Text(
-          'Up count',
-          style: TextStyle(fontSize: 24),
-        ),),
-            ),),);
+    return Column(
+      children:widgets,
+    );
   }
 }
